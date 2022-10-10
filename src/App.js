d3756e1864d12d71bca899085622c0a6eab916fb
soumik825 <allhafesh825@gmail.com>
2022-10-11 01:01:12 +0600

    added some components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Main from './Layout/Main';
+import Blogs from './Components/Blogs/Blogs';
+import Topics from './Components/Topics/Topics';
+import Statistics from './Components/Statistics/Statistics';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+
+        {
+          path: '/topics',
+          element: <Topics></Topics>
+        },
+        {
+          path: '/statistics',
+          element: <Statistics></Statistics>
+        },
+        {
+          path: '/blogs',
+          element: <Blogs></Blogs>
+        }
+      ]
+    },
+
+  ])
   return (
     <div>
-
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
 
 const Header = () => {
     return (
         <div>
             <nav className='header'>
-
+                <div className='name'><h1>Edu Quizes</h1></div>
+                <div className='link'>
+                    <Link to="/">Home</Link>
+                    <Link to="/topics">Topics</Link>
+                    <Link to="/statistics">Statistics</Link>
+                    <Link to="/blogs">Blogs</Link>
+                </div>
             </nav>
+            <div>
+
+            </div>
         </div>
     );
 };
